56c7eabbe93894d2ddd13cab73a64a22922bc6c0
Maria Lindling <maria.neith.lindling@protonmail.com>
2025-04-16 14:07:41 +0200

    trigger areas and movement slowing

--- Assets/BaseCharacterController.cs
+++ Assets/BaseCharacterController.cs
@@ -10,9 +10,13 @@ public class BaseCharacterController : MonoBehaviour
     [SerializeField] float movementSpeed;
     private Rigidbody2D rigidBody;
 
+    private bool isSlowed;
+    [Range(0,1)][SerializeField] private float slowedFactor;
+
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        isSlowed = false;
     }
 
     // Start is called before the first frame update
@@ -24,11 +28,31 @@ public class BaseCharacterController : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        rigidBody.AddForce((Vector3)movementInput * movementSpeed);
+        //rigidBody.AddForce((Vector3)movementInput * movementSpeed);
+
+        transform.Translate(
+            new Vector3(movementInput.x,movementInput.y,0)
+                * Time.deltaTime
+                * (isSlowed ? movementSpeed * slowedFactor : movementSpeed)
+        );
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision detected with " + collision.gameObject.name);
+
+        if (collision.gameObject.CompareTag("Swamp")) isSlowed = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)  { }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Swamp")) isSlowed = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Swamp")) isSlowed = false;
     }
 }
